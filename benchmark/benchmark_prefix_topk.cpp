990989b111966c2796672972f9ce2b79698225f9
Giulio Ermanno Pibiri <jeis90@gmail.com>
2019-10-09 10:20:55 +0200

    take whole time

--- benchmark/benchmark_prefix_topk.cpp
+++ benchmark/benchmark_prefix_topk.cpp
@@ -9,7 +9,7 @@ using namespace autocomplete;
 template <typename Index>
 void benchmark_prefix_topk(char const* binary_filename, uint32_t k,
                            uint32_t max_num_queries,
-                           essentials::json_lines& breakdowns) {
+                           essentials::json_lines& breakdowns, bool breakdown) {
     Index autocomp;
     essentials::logger("loading data structure from disk...");
     essentials::load(autocomp, binary_filename);
@@ -22,31 +22,48 @@ void benchmark_prefix_topk(char const* binary_filename, uint32_t k,
         load_queries(queries, max_num_queries, true, std::cin);
     essentials::logger("loaded " + std::to_string(num_queries) + " queries");
 
-    essentials::logger("benchmarking prefix_topk queries...");
-    std::vector<timer_type> timers(4);
-    uint64_t reported_strings = 0;
-    for (uint32_t run = 0; run != runs; ++run) {
-        for (auto& query : queries) {
-            auto it = autocomp.prefix_topk(query, k, timers);
-            reported_strings += it.size();
-        }
-    }
-    essentials::logger("DONE");
-    std::cout << reported_strings << std::endl;
-
     auto ns_x_query = [&](double time) {
         return uint64_t(time / (runs * num_queries) * 1000);
     };
 
-    breakdowns.add("num_queries", std::to_string(num_queries));
-    breakdowns.add("parsing_ns_per_query",
-                   std::to_string(ns_x_query(timers[0].elapsed())));
-    breakdowns.add("completions_search_ns_per_query",
-                   std::to_string(ns_x_query(timers[1].elapsed())));
-    breakdowns.add("topk_rmq_ns_per_query",
-                   std::to_string(ns_x_query(timers[2].elapsed())));
-    breakdowns.add("reporting_ns_per_query",
-                   std::to_string(ns_x_query(timers[3].elapsed())));
+    essentials::logger("benchmarking prefix_topk queries...");
+    uint64_t reported_strings = 0;
+
+    if (breakdown) {
+        std::vector<timer_type> timers(4);
+        for (uint32_t run = 0; run != runs; ++run) {
+            for (auto& query : queries) {
+                auto it = autocomp.prefix_topk(query, k, timers);
+                reported_strings += it.size();
+            }
+        }
+        essentials::logger("DONE");
+        std::cout << reported_strings << std::endl;
+        breakdowns.add("num_queries", std::to_string(num_queries));
+        breakdowns.add("parsing_ns_per_query",
+                       std::to_string(ns_x_query(timers[0].elapsed())));
+        breakdowns.add("completions_search_ns_per_query",
+                       std::to_string(ns_x_query(timers[1].elapsed())));
+        breakdowns.add("topk_rmq_ns_per_query",
+                       std::to_string(ns_x_query(timers[2].elapsed())));
+        breakdowns.add("reporting_ns_per_query",
+                       std::to_string(ns_x_query(timers[3].elapsed())));
+    } else {
+        essentials::timer_type timer;
+        timer.start();
+        for (uint32_t run = 0; run != runs; ++run) {
+            for (auto& query : queries) {
+                auto it = autocomp.prefix_topk(query, k);
+                reported_strings += it.size();
+            }
+        }
+        timer.stop();
+        essentials::logger("DONE");
+        std::cout << reported_strings << std::endl;
+        breakdowns.add("num_queries", std::to_string(num_queries));
+        breakdowns.add("ns_per_query",
+                       std::to_string(ns_x_query(timer.elapsed())));
+    }
 }
 
 int main(int argc, char** argv) {
@@ -54,7 +71,7 @@ int main(int argc, char** argv) {
     if (argc < mandatory + 1) {
         std::cout << argv[0]
                   << " <type> <k> <binary_filename> <num_terms_per_query> "
-                     "<max_num_queries> < queries"
+                     "<max_num_queries> --breakdown < queries"
                   << std::endl;
         return 1;
     }
@@ -65,17 +82,25 @@ int main(int argc, char** argv) {
     std::string num_terms_per_query(argv[4]);
     uint32_t max_num_queries = std::atoi(argv[5]);
 
+    bool breakdown = false;
+    for (int i = mandatory + 1; i != argc; ++i) {
+        if (std::string(argv[i]) == "--breakdown") {
+            breakdown = true;
+        }
+    }
+
     essentials::json_lines breakdowns;
     breakdowns.new_line();
     breakdowns.add("num_terms_per_query", num_terms_per_query);
 
     if (type == "type1") {
         benchmark_prefix_topk<uncompressed_autocomplete_type>(
-            binary_filename, k, max_num_queries, breakdowns);
+            binary_filename, k, max_num_queries, breakdowns, breakdown);
     } else if (type == "type2") {
         benchmark_prefix_topk<uncompressed_autocomplete_type2>(
-            binary_filename, k, max_num_queries, breakdowns);
+            binary_filename, k, max_num_queries, breakdowns, breakdown);
     } else {
+        std::cout << "error: unknown type '" << type << "'" << std::endl;
         return 1;
     }
 
