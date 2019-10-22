86162097890ad0e2f36635852319c8332e35a0d1
Giulio Ermanno Pibiri <jeis90@gmail.com>
2019-10-22 10:29:18 +0200

    script for benchmarking locate_prefix

--- script/collect_locate_prefix_results_by_varying_percentage.py
+++ script/collect_locate_prefix_results_by_varying_percentage.py
@@ -10,6 +10,6 @@ output_filename = dataset_name + "." + type + ".locate_prefix.timings.json"
 percentages = ["0.0", "0.25", "0.50", "0.75"]
 
 for perc in percentages:
-    for terms in range(2,8): # (1,8)
+    for terms in range(1,8):
         os.system("../build/benchmark_locate_prefix " + type + " " + collection_basename + " " + str(terms) + " " + str(num_queries) + " " + perc + " < ../test_data/" + dataset_name + "/" + dataset_name + ".completions.length=" + str(terms) + ".shuffled 2>> " + output_filename)
     os.system("../build/benchmark_locate_prefix " + type + " " + collection_basename + " 8+ " + str(num_queries) + " " + perc + " < ../test_data/" + dataset_name + "/" + dataset_name + ".completions.length=8+.shuffled 2>> " + output_filename)
